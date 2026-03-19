6395848078fabe47ae857c4ff13ed172a4f08fbd
Daniel <jilek.d@northeastern.edu>
2026-03-19 17:23:51 -0400

    Fix MongoDB connection

---
+++ backend/db/dbConn.js
@@ -0,0 +1,21 @@
+import {MongoClient} from "mongodb";
+
+let client;
+
+export const connectDB = () => {
+    try {
+        client = new MongoClient(process.env.DATABASE_URI);
+        client.connect();
+        return client;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+export const getDB = () => {
+    if (!client) {
+        client = connectDB();
+    }
+    return client.db("dev");
+}
--- backend/server.js
+++ backend/server.js
@@ -5,34 +5,15 @@ import carsRoutes from "./routes/carRoutes.js";
 import salesRoutes from "./routes/salesRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import passport from "passport";
-
 import bcrypt from "bcrypt";
+import { connectDB } from "./db/dbConn.js";
 
-
+const PORT = process.env.PORT || 10000;
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
-
-const PORT = 3000;
-const MONGO_URL = "mongodb://127.0.0.1:27017";
-const DB_NAME = "carDB";
-const client = new MongoClient(MONGO_URL);
-
-let db;
-
-async function connectDB() {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB");
-    db = client.db(DB_NAME);
-  } catch (error) {
-    console.error("MongoDB connection failed", error);
-  }
-}
-
-await connectDB();
+const client = connectDB();
 
 app.use("/api/cars", carsRoutes(db));
 app.use('/api/auth', userRoutes(db));
