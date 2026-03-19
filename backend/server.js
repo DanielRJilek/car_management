6efbf36f0b04590326ef3817d13d4c7235efdfe0
Daniel <jilek.d@northeastern.edu>
2026-03-19 17:54:51 -0400

    CORS fix

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ const app = express();
 app.use(express.json());
 const client = connectDB();
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
